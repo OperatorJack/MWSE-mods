37d91a7d6b735a10f8abffb1bf054142ae7810bf
OperatorJack <thepaladinof@gmail.com>
2021-07-10 16:03:03 -0400

    Update Better Buoyancy to account for paralysis, thanks Null!

--- Better Buoyancy/MWSE/mods/OperatorJack/BetterBuoyancy/main.lua
+++ Better Buoyancy/MWSE/mods/OperatorJack/BetterBuoyancy/main.lua
@@ -6,7 +6,27 @@ end)
 local config = require("OperatorJack.BetterBuoyancy.config")
 
 event.register("simulate", function(e)
-    if (tes3.mobilePlayer.underwater == true and config.underwaterControlsEnabled == true) then
+    if (tes3.mobilePlayer.encumbrance.normalized > 1.0 or tes3.mobilePlayer.paralyze > 0) then
+        return
+    end
+
+    if (tes3.mobilePlayer.levitate > 0 and config.levitationControlsEnabled == true) then
+        local delta = tes3.mobilePlayer.flySpeed
+
+        local inputController = tes3.worldController.inputController
+        local isUpPressed = inputController:keybindTest(tes3.keybind.jump, tes3.keyTransition.up)
+        local isDownPressed = inputController:keybindTest(tes3.keybind.sneak, tes3.keyTransition.up)
+
+        if (isUpPressed == true) then
+            tes3.mobilePlayer.velocity.z = delta
+            return
+        end
+        
+        if (isDownPressed == true and (tes3.player.position.z - tes3.mobilePlayer.lastGroundZ) >= delta) then
+            tes3.mobilePlayer.velocity.z = delta * -1
+            return
+        end
+    elseif (tes3.mobilePlayer.underwater == true and config.underwaterControlsEnabled == true) then
         local delta = e.delta * (tes3.mobilePlayer.isRunning and tes3.mobilePlayer.swimRunSpeed or tes3.mobilePlayer.swimSpeed)
 
         local inputController = tes3.worldController.inputController
@@ -34,24 +54,6 @@ event.register("simulate", function(e)
         
         if (isDownPressed == true and (tes3.player.position.z - tes3.mobilePlayer.lastGroundZ) >= delta) then
             tes3.player.position.z = tes3.player.position.z - delta
-            return
-        end
-    end
-
-    if (tes3.mobilePlayer.levitate > 0 and config.levitationControlsEnabled == true) then
-        local delta = tes3.mobilePlayer.flySpeed
-
-        local inputController = tes3.worldController.inputController
-        local isUpPressed = inputController:keybindTest(tes3.keybind.jump, tes3.keyTransition.up)
-        local isDownPressed = inputController:keybindTest(tes3.keybind.sneak, tes3.keyTransition.up)
-
-        if (isUpPressed == true) then
-            tes3.mobilePlayer.velocity.z = delta
-            return
-        end
-        
-        if (isDownPressed == true and (tes3.player.position.z - tes3.mobilePlayer.lastGroundZ) >= delta) then
-            tes3.mobilePlayer.velocity.z = delta * -1
             return
         end
     end
