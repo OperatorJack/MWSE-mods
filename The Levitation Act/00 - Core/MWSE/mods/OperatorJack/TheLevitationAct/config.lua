0a18e3022b685150e226ab0a90f95e2df2a4b0a9
OperatorJack <thepaladinof@gmail.com>
2021-10-06 11:31:20 -0400

    Update config.lua

--- The Levitation Act/00 - Core/MWSE/mods/OperatorJack/TheLevitationAct/config.lua
+++ The Levitation Act/00 - Core/MWSE/mods/OperatorJack/TheLevitationAct/config.lua
@@ -8,8 +8,12 @@ return mwse.loadConfig("The-Levitation-Act") or {
         ["tel aruhn"] = true,
         ["tel branora"] = true,
         ["tel fyr"] = true,
+        ["tower of tel fyr"] = true,
+        ["corprusarium"] = true,
+        ["corprusarium bowels"] = true,
         ["tel mora"] = true,
         ["tel uvirith"] = true,
         ["tel vos"] = true,
+        ["vos"] = true,
     }
 }
